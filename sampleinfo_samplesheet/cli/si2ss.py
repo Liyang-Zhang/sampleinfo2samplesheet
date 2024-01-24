f0ef1e3034e59ed05868303b0ae8ae2fd9b642dd
Liyang-Zhang <1219187848@qq.com>
2024-01-24 16:47:31 +0800

    Feat: restructure cli

--- sampleinfo_samplesheet/cli/si2ss.py
+++ sampleinfo_samplesheet/cli/si2ss.py
@@ -1,18 +1,58 @@
 import logging
+from pathlib import Path
 
 import typer
 
-import sampleinfo_samplesheet.cli.kunyuan as kunyuan
-from sampleinfo_samplesheet.logging.helper import config_console_debug_logger
+from sampleinfo_samplesheet.cli import helper
+from sampleinfo_samplesheet.logging.helper import (
+    LoggingLevel,
+    config_console_debug_logger,
+)
 
 log = logging.getLogger(__name__)
 config_console_debug_logger(__name__)
 
-SIG_KUNYUAN = "kunyuan"
-
 APP = typer.Typer()
+SIG = "kunyuan"
 
-APP.add_typer(kunyuan.APP, name=kunyuan.SIG)
+
+@APP.command(SIG)
+def palindrome_artifact(
+    _: bool = typer.Option(
+        None, "--version", callback=helper.version_callback, is_eager=True
+    ),
+    verbosity: LoggingLevel = typer.Option(
+        LoggingLevel.INFO,
+        "-v",
+        "--verbosity",
+        envvar=("_".join([SIG, "verbosity"])).upper(),
+        case_sensitive=False,
+        help="logging level",
+    ),
+    logfile: Path = typer.Option(
+        None,
+        "-l",
+        "--logfile",
+        file_okay=True,
+        dir_okay=False,
+        resolve_path=True,
+        envvar=("_".join([SIG, "logfile"])).upper(),
+        help="path to log file",
+    ),
+):
+    logging.basicConfig(level=verbosity.upper())
+    for _name in []:
+        logging.getLogger(_name).setLevel(logging.ERROR)
+    rfh = helper.get_rotating_file_handler(logfile=logfile)
+
+    if rfh:
+        rfh.setLevel(verbosity.upper())
+        log.addHandler(rfh)
+        logging.getLogger("sampleinfo_samplesheet.cli.helper").addHandler(rfh)
+        logging.getLogger("sampleinfo_samplesheet.logging.helper").addHandler(rfh)
+        logging.getLogger().addHandler(rfh)
+
+    # TODO Add main script here
 
 
 def main():
