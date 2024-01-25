bfbc58f64d4d64388d030d6f798bb4210151dd8b
Liyang-Zhang <1219187848@qq.com>
2024-01-25 13:23:53 +0800

    Feat: Change interface, enable mutiple input excel files

--- sampleinfo_samplesheet/cli/si2ss.py
+++ sampleinfo_samplesheet/cli/si2ss.py
@@ -1,5 +1,6 @@
 import logging
 from pathlib import Path
+from typing import List
 
 import typer
 
@@ -9,23 +10,54 @@ from sampleinfo_samplesheet.logging.helper import (
     config_console_debug_logger,
 )
 
+# from typing_extensions import Annotated
+
+
 log = logging.getLogger(__name__)
 config_console_debug_logger(__name__)
 
 APP = typer.Typer()
-SIG = "kunyuan"
+KUNYUAN = "kunyuan"
 
 
-@APP.command(SIG)
-def palindrome_artifact(
+@APP.command(KUNYUAN)
+def kunyuan(
     _: bool = typer.Option(
         None, "--version", callback=helper.version_callback, is_eager=True
+    ),
+    input: List[Path] = typer.Option(
+        ...,
+        "-i",
+        "--input",
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+        help="Path to the input excel file",
+    ),
+    output: Path = typer.Option(
+        ...,
+        "-o",
+        "--output",
+        exists=False,
+        file_okay=False,
+        dir_okay=True,
+        help="Directory to the output sample sheet.",
+    ),
+    config_file: Path = typer.Option(
+        None,
+        "-c",
+        "--config",
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+        resolve_path=True,
+        help="Path to the parameter config file.",
     ),
     verbosity: LoggingLevel = typer.Option(
         LoggingLevel.INFO,
         "-v",
         "--verbosity",
-        envvar=("_".join([SIG, "verbosity"])).upper(),
+        envvar=("_".join([KUNYUAN, "verbosity"])).upper(),
         case_sensitive=False,
         help="logging level",
     ),
@@ -36,7 +68,7 @@ def palindrome_artifact(
         file_okay=True,
         dir_okay=False,
         resolve_path=True,
-        envvar=("_".join([SIG, "logfile"])).upper(),
+        envvar=("_".join([KUNYUAN, "logfile"])).upper(),
         help="path to log file",
     ),
 ):
@@ -53,6 +85,15 @@ def palindrome_artifact(
         logging.getLogger().addHandler(rfh)
 
     # TODO Add main script here
+    log.info(input)
+    log.info(output)
+
+
+@APP.callback()
+def callback():
+    """
+    Convert a sequencing sample info excel to a sample sheet for bcl2fastq
+    """
 
 
 def main():
